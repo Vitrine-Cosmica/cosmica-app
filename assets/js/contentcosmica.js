e88483e8c7a33ec1bae3413db3661d68604e5954
alelelef <alefchaves.cap@gmail.com>
2024-11-17 06:55:16 -0300

    pagina cosmica atualizada

--- assets/js/contentcosmica.js
+++ assets/js/contentcosmica.js
@@ -11,19 +11,104 @@ document.addEventListener('DOMContentLoaded', function() {
     
 
     pageCosmica.innerHTML = `
-    <img src="assets/img/grup.svg" alt="" class="union">
+
+<div class="allcontentcosmica">
+
         <h1 class="titulo-cosmica">Cósmica upcycle</h1>
         <h2 class="subtitulo-cosmica">Resgatando o Valor do Descartável</h2>
         <p class="texto-upcycle">
-            Em 2020, a Cósmica começou. <br>
-            Sem muitos planos sobre o que se tornaria, nasceu como um pequeno empreendimento de uma carioca que, há pouco tempo, havia se aventurado a viver em terras do sul. <br>
-            Sempre enxerguei valor em coisas que as pessoas já não viam mais. Sempre olhei para o “lixo” de um outro ângulo... o de uma oportunidade. Talvez a falta de grana para comprar materiais nos leve a olhar para esses materiais chamados de “lixo” com muito mais criatividade. As primeiras bolsas saíram de quatro calças jeans que garimpei em um bazar. A primeira mochila foi feita com retalhos de roupas que eu mesma havia produzido no curso de costura. E assim começou o que seria a Cósmica hoje. <br>
-            Confesso que nem conhecia o termo “upcycle” na época. <br>
-            Nossa prioridade é utilizar ao máximo materiais já existentes, desde os tecidos até papéis para embalagens e etiquetas, sempre minimizando o uso de plástico. Para aviamentos, compramos apenas a quantidade necessária para cada coleção, escolhendo materiais de alta qualidade que garantem resistência e durabilidade ao produto final. Dessa forma, otimizamos os recursos e reduzimos o impacto ambiental em todas as etapas do processo.
+            Em 2020, a Cósmica começou. <br> Sem muitos planos sobre o que se tornaria, nasceu como um pequeno empreendimento de uma carioca que, há pouco tempo, havia se aventurado a viver em terras do sul. <br> Sempre enxerguei valor em coisas que as pessoas já não viam mais. Sempre olhei para o “lixo” de um outro ângulo... o de uma oportunidade. Talvez a falta de grana para comprar materiais nos leve a olhar para esses materiais chamados de “lixo” com muito mais criatividade. As primeiras bolsas saíram de quatro calças jeans que garimpei em um bazar. A primeira mochila foi feita com retalhos de roupas que eu mesma havia produzido no curso de costura. E assim começou o que seria a Cósmica hoje. <br> Confesso que nem conhecia o termo “upcycle” na época. <br> Nossa prioridade é utilizar ao máximo materiais já existentes, desde os tecidos até papéis para embalagens e etiquetas, sempre minimizando o uso de plástico. Para aviamentos, compramos apenas a quantidade necessária para cada coleção, escolhendo materiais de alta qualidade que garantem resistência e durabilidade ao produto final. Dessa forma, otimizamos os recursos e reduzimos o impacto ambiental em todas as etapas do processo.
         </p>
 
+
         <h1 class="titulo-galeria">Galeria Cósmica</h1>
-        
+
+        <section class="galeriaimg">
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img1.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img6.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img8.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img7.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img2.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img3.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img4.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img5.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img9.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img10.png">
+
+            </div>
+             <div class="itemgaleria">
+                <img src="assets/img/galeria/img11.png">
+
+            </div>
+             <div class="itemgaleria">
+                <img src="assets/img/galeria/img12.png">
+
+            </div>
+             <div class="itemgaleria">
+                <img src="assets/img/galeria/img13.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img14.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img15.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img16.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img17.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img18.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img19.png">
+
+            </div>
+            <div class="itemgaleria">
+                <img src="assets/img/galeria/img20.png">
+
+            </div>
+        </section>
+         
+
+
+  
     `;
 
 
